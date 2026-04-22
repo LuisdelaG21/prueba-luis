dacd1c356e2160cf66e8527def2493e138377b25
LuisdelaG21 <luisdxgauss2164@gmail.com>
2026-04-21 21:21:03 -0600

    Modificacion de suma.py

--- suma.py
+++ suma.py
@@ -6,3 +6,7 @@ print ("ingrese el segundo numero")
 num2 = input()  
 suma = int(num1) + int(num2)
 print ("la suma es: " + str(suma))
+
+print ("suma de tres numeros")
+print ("ingrese el primer numero")      
+num1 = input()
