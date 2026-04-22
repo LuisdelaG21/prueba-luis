3f9990676679326914489db29fb855817256e17c
LuisdelaG21 <luisdxgauss2164@gmail.com>
2026-04-21 21:01:35 -0600

    Mi segundo commit

---
+++ suma.py
@@ -0,0 +1,8 @@
+print ("suma dos numeros")
+print ("ingrese el primer numero")
+num1 = input()
+
+print ("ingrese el segundo numero")
+num2 = input()  
+suma = int(num1) + int(num2)
+print ("la suma es: " + str(suma))
